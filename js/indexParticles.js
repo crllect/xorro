45968bc719cdd4f4dcbeed8c175d49db484d0193
Crllect <crllect@proton.me>
2023-11-26 01:39:03 -0500

    Basically re-wrote all of the css to be completely procedural. Dont know why, but this will make this less tedious in the long run.

--- js/indexParticles.js
+++ js/indexParticles.js
@@ -31,7 +31,7 @@ particlesJS("particles-js", {
           }
         },
         "size": {
-          "value": 3,
+          "value": 4,
           "random": true,
           "anim": {
             "enable": false,
