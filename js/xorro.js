961e835588f4252fc9c10002c2b5d9b907c0f00c
Crllect <crllect@proton.me>
2023-12-03 01:58:17 -0500

    created a very basic implamentation of the menu
    This entire project was just my attempt to improve at scss and server sided elements, but honestly, its been pretty smooth sailing. I mean, just my menu is like 500 lines of scss. Definitely less then optimized.

--- js/xorro.js
+++ js/xorro.js
@@ -2,12 +2,18 @@ document.querySelector('.menu').addEventListener('click', function() {
     this.classList.toggle('menuOpen');
     var mainWindow = document.querySelector('.mainWindow').classList;
     var waybar = document.querySelector('.waybar').classList;
+    var fullMenu = document.querySelector('.fullMenu').classList;
+    var body = document.body.classList;
   
     if (this.classList.contains('menuOpen')) {
       mainWindow.add('menuOpen-mainWindow');
       waybar.add('menuOpen-waybar');
+      fullMenu.add('menuOpen-fullMenu');
+      body.add('menuOpen-body');
     } else {
       mainWindow.remove('menuOpen-mainWindow');
       waybar.remove('menuOpen-waybar');
+      fullMenu.remove('menuOpen-fullMenu');
+      body.remove('menuOpen-body');
     }
   });
