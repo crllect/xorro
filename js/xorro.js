4a68f33fdec671c6b7e624b4416eb9b7ac490393
Crllect <crllect@proton.me>
2023-12-03 02:56:25 -0500

    added a couple of presets and put an iframe to copy.sh as a placeholder. I am going to put rammerhead on this soon

--- js/xorro.js
+++ js/xorro.js
@@ -1,17 +1,20 @@
 document.querySelector('.menu').addEventListener('click', function() {
     this.classList.toggle('menuOpen');
-    var mainWindow = document.querySelector('.mainWindow').classList;
+    // var mainWindow = document.querySelector('.mainWindow').classList;
+    var iframeWindow = document.querySelector('.iframeWindow').classList;
     var waybar = document.querySelector('.waybar').classList;
     var fullMenu = document.querySelector('.fullMenu').classList;
     var body = document.body.classList;
   
     if (this.classList.contains('menuOpen')) {
-      mainWindow.add('menuOpen-mainWindow');
+      // mainWindow.add('menuOpen-mainWindow');
+      iframeWindow.add('menuOpen-iframeWindow');
       waybar.add('menuOpen-waybar');
       fullMenu.add('menuOpen-fullMenu');
       body.add('menuOpen-body');
     } else {
-      mainWindow.remove('menuOpen-mainWindow');
+      // mainWindow.remove('menuOpen-mainWindow');
+      iframeWindow.remove('menuOpen-iframeWindow');
       waybar.remove('menuOpen-waybar');
       fullMenu.remove('menuOpen-fullMenu');
       body.remove('menuOpen-body');
