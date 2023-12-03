de80695018a48a2e6bea58a8c1e4d78a995f218f
Crllect <crllect@proton.me>
2023-12-02 19:28:28 -0500

    debugging

--- js/xorro.js
+++ js/xorro.js
@@ -1,13 +1,13 @@
 document.querySelector('.menu').addEventListener('click', function() {
     this.classList.toggle('menuOpen');
-    var mainWindow = document.querySelector('.mainWindow');
-    var waybar = document.querySelector('.waybar');
+    var mainWindow = document.querySelector('.mainWindow').classList;
+    var waybar = document.querySelector('.waybar').classList;
   
     if (this.classList.contains('menuOpen')) {
-      mainWindow.classList.add('menuOpen-mainWindow');
-      waybar.classList.add('menuOpen-waybar');
+      mainWindow.add('menuOpen-mainWindow');
+      waybar.add('menuOpen-waybar');
     } else {
-      mainWindow.classList.remove('menuOpen-mainWindow');
-      waybar.classList.remove('menuOpen-waybar');
+      mainWindow.remove('menuOpen-mainWindow');
+      waybar.remove('menuOpen-waybar');
     }
   });
